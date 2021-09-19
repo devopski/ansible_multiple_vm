603d9a2db23e8e4a75752d1b1eee921ecc64be80
devopski <dawid@devopski.com>
2021-09-19 17:30:59 +0200

    removed sshpass installation

--- vagrant/prepare_control_node.sh
+++ vagrant/prepare_control_node.sh
@@ -2,7 +2,8 @@
 
 echo "[ANSIBLE INSTALATION]..."
 sudo apt update > /dev/null 2>&1
+export ANSIBLE_HOST_KEY_CHECKING=False
 sudo apt install software-properties-common -y > /dev/null 2>&1
 sudo add-apt-repository --yes --update ppa:ansible/ansible > /dev/null 2>&1
 sudo apt install ansible -y > /dev/null 2>&1
-export ANSIBLE_HOST_KEY_CHECKING=False
+sudo apt install sshpass -y > /dev/null 2>&1
