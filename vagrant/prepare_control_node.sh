a72ff765fdf2091518bac709b4ec0d10bc21a119
devopski <77720864+devopski@users.noreply.github.com>
2021-09-19 16:34:45 +0200

    export as vagrant user

--- vagrant/prepare_control_node.sh
+++ vagrant/prepare_control_node.sh
@@ -6,4 +6,4 @@ sudo apt install software-properties-common -y > /dev/null 2>&1
 sudo add-apt-repository --yes --update ppa:ansible/ansible > /dev/null 2>&1
 sudo apt install ansible -y > /dev/null 2>&1
 sudo apt install sshpass -y > /dev/null 2>&1
-export ANSIBLE_HOST_KEY_CHECKING=False
+su - vagrant -c "export ANSIBLE_HOST_KEY_CHECKING=False"
