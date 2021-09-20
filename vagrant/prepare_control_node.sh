277356f71536da524dba1a9871a34dc7c880837a
devopski <dawid@devopski.com>
2021-09-20 20:35:17 +0200

    host key checking off

--- vagrant/prepare_control_node.sh
+++ vagrant/prepare_control_node.sh
@@ -7,3 +7,4 @@ sudo apt install software-properties-common -y > /dev/null 2>&1
 sudo add-apt-repository --yes --update ppa:ansible/ansible > /dev/null 2>&1
 sudo apt install ansible -y > /dev/null 2>&1
 sudo apt install sshpass -y > /dev/null 2>&1
+sudo sed -i 's|[#]*#host_key_checking = False|host_key_checking = False|g' /etc/ansible/ansible.cfg
